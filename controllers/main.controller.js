b713f3b15e0c7db5e1e7bc7d7b412719a7226931
bomi <bonita9931@gmail.com>
2021-04-03 20:52:28 +0900

    Feat: 메인페이지 상단 검색창 구현

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -29,7 +29,6 @@ const getMajor = async (req, res, next) => {
 }
 
 
-
 const getCollege = async (req, res, next) => {
     try {
         const result2 = await model.College.findAll({
@@ -52,7 +51,6 @@ const getCollege = async (req, res, next) => {
         console.log("error!!")
     }
 }
-
 
 
 const getCentral = async (req, res, next) => {
@@ -78,16 +76,34 @@ const getCentral = async (req, res, next) => {
     }
 }
 
+const renameKey = (object, key, newKey) => {
+    return {'id':object['id'], [newKey] : object[key]}
+};
+
 const getSearchCategory = async (req, res, next) => {
     try {
-
-        const result2 = await model.Central.findAll({
-
+        let centralResult = await model.Central.findAll();
+        let collegeResult = await model.College.findAll();
+        let majorResult = await model.Major.findAll({
+            attributes: ['id', 'majorName']
         })
 
+        centralResult = centralResult.map((res) =>
+            renameKey(res['dataValues'], "centralName", "name")
+        )
+        collegeResult = collegeResult.map((res) => {
+            return renameKey(res['dataValues'], "collegeName", "name")
+        })
+        majorResult = majorResult.map((res) => {
+            return renameKey(res['dataValues'], "majorName", "name")
+        })
 
+        let result = collegeResult.concat(centralResult);
+        result = result.concat(majorResult);
+
+        return res.json(result);
     } catch (e) {
-        console.log("error!!")
+        console.log(e)
     }
 }
 
--- routes/main.js
+++ routes/main.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/central-organization', index.getCentral)
 router.get('/college', index.getCollege)
 router.get('/major', index.getMajor)
+router.get('/search', index.getSearchCategory)
 
 module.exports = router;
