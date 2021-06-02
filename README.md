24a41fd2ed2728acf0d94b7df43a1b88a90073d9
JangBomi <bonita9931@gmail.com>
2021-06-02 23:18:47 +0900

    modify - app.js 수정

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 npx sequelize db:create
 
 npm start
+
+**배포 에러보기**<br>
+sudo npx pm2 logs --err
+
+**배포 재시작**<br>
+sudo npx pm2 reload all
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+const app = require('./app').app;
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
