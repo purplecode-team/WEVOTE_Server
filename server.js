5b29c1f002d2a6060a79c9a6c16be1b0b3e367e6
JangBomi <bonita9931@gmail.com>
2021-06-04 19:58:04 +0900

    modify - package.json npm start 수정

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기중');
-});
+// app.listen(app.get('port'), () => {
+//     console.log(app.get('port'), '번 포트에서 대기중');
+// });
 
 const exec = require('child_process').exec;
 const path = require('path');
