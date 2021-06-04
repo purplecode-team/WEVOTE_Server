5b902b90a80a164dc5b9a4a6db1010600013576c
JangBomi <bonita9931@gmail.com>
2021-06-04 22:05:37 +0900

    modify - 배포  수정

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-const exec = require('child_process').exec;
+//const exec = require('child_process').exec;
 // const path = require('path');
 const client = exec('npm run start', {
     windowsHide: true,
