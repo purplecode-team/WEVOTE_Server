cbbd94481540c3496b5b73884b355332eadcb376
JangBomi <bonita9931@gmail.com>
2021-07-03 02:20:34 +0900

    Feat- 메인페이지 캘린더 이미지  경로 불러오기

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -125,6 +125,19 @@ const getBanner = async (req, res, next) => {
     }
 }
 
+const getCalendar = async (req, res, next) => {
+    try {
+        const calendar = await model.Calendar.findOne({
+                order: [['id', 'DESC']],
+            }
+        );
+
+        return res.json(calendar);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 const getElection = async (req, res, next) => {
     try {
         // const Op = Sequelize.Op;
@@ -191,4 +204,4 @@ const getMain = async (req, res, next) => {
 }
 
 
-module.exports = {getMajor, getCollege, getCentral, getSearchCategory, getElection, getMain, getBanner}
+module.exports = {getMajor, getCollege, getCentral, getSearchCategory, getElection, getMain, getBanner, getCalendar}
