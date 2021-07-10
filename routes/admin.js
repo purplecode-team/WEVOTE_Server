160d12637ca41a47a6c283ed85b073cacd447e58
JangBomi <bonita9931@gmail.com>
2021-07-10 22:54:46 +0900

    modify - env 파일 수정

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,14 @@ const admin = require("../controllers/admin.controller");
 const router = express.Router();
 
 
-router.post('/category', admin.registerCategory)
-router.post('/banner', admin.registerBanner)
-router.delete('/banner/:id', admin.deleteBanner)
-router.patch('/banner/:id', admin.updateBanner)
+router.post('/category', admin.registerCategory);
+router.post('/banner', admin.registerBanner);
+router.delete('/banner/:id', admin.deleteBanner);
+router.patch('/banner/:id', admin.updateBanner);
 router.post('/calendar', admin.registerCalendar);
+router.delete('/calendar/:id', admin.deleteCalendar);
 router.post('/election-info', admin.registerInfo);
 router.get('/info-list', admin.getInfoImgList);
 router.delete('/info/:id', admin.deleteInfoImg);
+router.post('/central/candidate', admin.registerCandidate)
 module.exports = router;
